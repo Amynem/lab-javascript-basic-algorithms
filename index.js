d9905a6e024a04a8657b0497ced79841f907fda1
Amynem <amy.lauren98@hotmail.com>
2024-09-27 23:45:44 +0200

    lab

--- index.js
+++ index.js
@@ -41,14 +41,19 @@ for ( let i = hacker1.length - 1; i >= 0; i-- ) {
 }
 console.log(nameBackwards);
 
-// 3.3 no se platearlo.
+// 3.3 
+function nameComesFirst() {
+    const nameFirst = hacker1.localeCompare(hacker2);
+    
+    if (nameFirst < 0) {
+        console.log("The driver's name goes first.");
+    } else if ( nameFirst > 0){
+        console.log("Yo, the navigator goes first, definitely.");
+    } else {
+        console.log("What?! You both have the same name?");
+    }
+}
 
-
+nameComesFirst(hacker1, hacker2);
 
 // bonus
-const longText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque sit amet metus vitae odio sagittis pharetra at ac diam. Vivamus a efficitur magna, et dapibus ipsum. Nullam euismod sapien bibendum leo congue volutpat. Interdum et malesuada fames ac ante ipsum primis in faucibus. Praesent non tellus pulvinar, ultricies nulla et, feugiat sapien. Ut dapibus venenatis rutrum. Ut luctus sem massa, sit amet dignissim tortor vestibulum aliquet. Donec maximus sapien eget nibh varius, eu posuere risus bibendum. Aliquam dapibus aliquam lorem, et varius neque fermentum eget.
-
-Phasellus cursus, felis sed condimentum lobortis, leo risus hendrerit diam, molestie efficitur mauris lacus ut dui. Aenean sagittis nisl consequat condimentum dapibus. Cras eu felis tristique, cursus velit sollicitudin, congue turpis. Nulla sit amet dolor lacus. Quisque interdum vitae ligula id semper. Duis pulvinar eget urna in imperdiet. Mauris lacus tortor, sagittis in libero ut, posuere pretium elit. Donec feugiat ex tortor, non fringilla ante pulvinar ut.
-
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vitae dui pharetra, mollis ante dictum, dignissim felis. Fusce mollis justo eu ante ultricies condimentum. Nunc fringilla fermentum risus, vitae imperdiet tortor maximus eget. Nulla ullamcorper non turpis id maximus. Sed ultricies eros non tincidunt pulvinar. Ut ex mi, pulvinar et nisl vel, sagittis consectetur dui."
-
